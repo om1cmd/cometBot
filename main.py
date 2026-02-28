374787c9816e9e54597b1072edae73565728c90a
Marek Mička <marek.micka04@gmail.com>
2026-02-28 19:27:33 +0100

    added embed generation and descriptive text

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ WEBHOOK = getenv('WEBHOOK')
 
 URL1 = 'https://cobs.si/api/comet_list.api?cur-mag='
 URL2 = '&page=1'
-MAG = '15' # filter comets by max current magnitude
-URL = URL1 + MAG + URL2
+MAG1 = '15' # filter comets by max current magnitude
+MAG2 = '10'
+URL = URL1 + MAG1 + URL2
+TODAY = datetime.today().strftime('%Y-%m-%d')
 
 data = requests.get(URL).json()
 comets = data['objects'] # returns a list of dicts with comet info
-print(len(comets))
 
 # filter comets by peak_mag_date
 oneWeekAgo = datetime.today() - timedelta(days=7)
@@ -23,13 +24,13 @@ filteredComets = []
 for comet in comets:
     peak_mag_date = datetime.strptime(comet['peak_mag_date'], '%Y-%m-%d')
     peak_mag = comet['peak_mag']
-    if peak_mag_date >= oneWeekAgo and peak_mag < '10':
+    if peak_mag_date >= oneWeekAgo and peak_mag < MAG2:
         filteredComets.append(comet)
 
 #sort comets by peak_mag_date
 sortedComets = sorted(filteredComets, key=lambda comet: datetime.strptime(comet['peak_mag_date'], '%Y-%m-%d'), reverse=False)
 
-def table(sortedComets):
+def table(sortedComets=sortedComets):
     # put each column into a separate list
     fullnames = [i['fullname'] for i in sortedComets]
     currMags = [i['current_mag'] for i in sortedComets]
@@ -55,8 +56,33 @@ def table(sortedComets):
     table = '\n'.join(['```', header, separator] + rows + ['```']) # put all elements into one list and join them with newline characters
     return table
 
-content = table(sortedComets)
-payload = {'content': content}
+def embed(sortedComets=sortedComets):
+    embeds = []
+
+    def buildEmbed(comet):
+        embed = {
+            'title': comet['fullname'],
+            'color': 0x33eeff,
+            'fields': [
+                {'name': 'Current mag', 'value': comet['current_mag'], 'inline': True},
+                {'name': 'Peak mag', 'value': comet['peak_mag'], 'inline': True},
+                {'name': 'Peak mag date', 'value': comet['peak_mag_date'], 'inline': True}
+            ]
+        }
+        return embed
+
+    for comet in sortedComets:
+        embeds.append(buildEmbed(comet))
+    
+    return embeds
+
+title = f'# Comet report from {TODAY}\n'
+subtitle = f'### Showing comets with current mag at least {MAG1} and peak mag at most {MAG2}\n'
+vspace = '\n### Same information in embeds for better rendering on mobile\n'
+content = title + subtitle + table() + vspace
+
+embeds = embed()
+payload = {'content': content, 'embeds': embeds}
 
 response = requests.post(WEBHOOK, json=payload)
 print(response.status_code)
